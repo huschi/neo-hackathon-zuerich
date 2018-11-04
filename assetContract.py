d2a13c97964db825c0b731693b4b71b6a8756c12
Tomasik <tomasik@swissquant.com>
2018-11-04 16:53:38 +0100

    Update contract permissions

--- assetContract.py
+++ assetContract.py
@@ -1,17 +1,20 @@
 from boa.interop.Neo.Storage import Get,Put,Delete,GetContext
 from boa.interop.Neo.Runtime import Serialize, Deserialize
+from boa.blockchain.vm.Neo.Runtime import CheckWitness
 
 def Main(operation, args):
     ctx = GetContext()
 
     if operation == 'createOrder':
-        if len(args) > 2:
+        if len(args) > 3:
             orderId = strVal(args[0])
             senderKey = strVal(args[1])
             receiverKey = strVal(args[2])
+            middleManKey = strVal(args[3])
             order = {
                 'senderKey': senderKey,
                 'receiverKey': receiverKey,
+                'middleManKey': middleManKey,
                 'snapshots': []
             }
             serializedOrder = Serialize(order)
@@ -24,7 +27,7 @@ def Main(operation, args):
             orderId = strVal(args[0])
             snapshotHash = strVal(args[1])
             order = Get(ctx, orderId)
-            if order != None: 
+            if order != None and hasUpdatePermissions(order): 
                 order['snapshots'].append(snapshotHash)
                 serializedOrder = Serialize(order)
                 Put(ctx, orderId, serializedOrder)
@@ -48,3 +51,6 @@ def Main(operation, args):
 
 def strVal(inputArrayElement):
     substr(inputArrayElement, 1, len(inputArrayElement)) # We prefix each variable in input array with : to avoid encoding
+
+def hasUpdatePermissions(order):
+    return CheckWitness(order.senderKey) or CheckWitness(order.receiverKey) or CheckWitness(order.middleManKey)
