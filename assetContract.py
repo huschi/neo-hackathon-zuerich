4e14ebd3467f468dbb2099b60137184ebdcb00ce
Tomasik <tomasik@swissquant.com>
2018-11-04 17:34:20 +0100

    Rename data structure

--- assetContract.py
+++ assetContract.py
@@ -5,52 +5,52 @@ from boa.blockchain.vm.Neo.Runtime import CheckWitness
 def Main(operation, args):
     ctx = GetContext()
 
-    if operation == 'createOrder':
+    if operation == 'createAsset':
         if len(args) > 3:
-            orderId = strVal(args[0])
+            assetId = strVal(args[0])
             senderKey = strVal(args[1])
             receiverKey = strVal(args[2])
             middleManKey = strVal(args[3])
-            order = {
+            asset = {
                 'senderKey': senderKey,
                 'receiverKey': receiverKey,
                 'middleManKey': middleManKey,
                 'snapshots': []
             }
-            serializedOrder = Serialize(order)
-            Put(ctx, orderId, serializedOrder)
-            return "Order created successfully"
-        return "Invalid number of args to create an order"
+            serializedAsset = Serialize(asset)
+            Put(ctx, assetId, serializedAsset)
+            return "Asset created successfully"
+        return "Invalid number of args to create an asset"
 
-    elif operation == 'updateOrder':
+    elif operation == 'updateAsset':
         if len(args) > 1:
-            orderId = strVal(args[0])
+            assetId = strVal(args[0])
             snapshotHash = strVal(args[1])
-            order = Get(ctx, orderId)
-            if order != None and hasUpdatePermissions(order): 
-                order['snapshots'].append(snapshotHash)
-                serializedOrder = Serialize(order)
-                Put(ctx, orderId, serializedOrder)
-                return "Order updated"
+            asset = Get(ctx, assetId)
+            if asset != None and hasUpdatePermissions(asset): 
+                asset['snapshots'].append(snapshotHash)
+                serializedAsset = Serialize(asset)
+                Put(ctx, assetId, serializedAsset)
+                return "Asset updated"
             else:
-                return "Order not found"
-        return "Invalid number of args to create an order"
+                return "Asset not found"
+        return "Invalid number of args to create an asset"
     
-    elif operation == 'getOrder':
+    elif operation == 'getAsset':
         if len(args) > 0:
-            orderId = strVal(args[0])
-            order = Get(ctx, orderId)
-            if order != None: 
-                deserializedOrder = Deserialize(order)
-                return deserializedOrder
+            assetId = strVal(args[0])
+            asset = Get(ctx, assetId)
+            if asset != None: 
+                deserializedAsset = Deserialize(asset)
+                return deserializedAsset
             else:
-                "Order not found"
-        return "Invalid number of args to get an order"
+                "Asset not found"
+        return "Invalid number of args to get an asset"
  
     return "No valid operation specified"
 
 def strVal(inputArrayElement):
     substr(inputArrayElement, 1, len(inputArrayElement)) # We prefix each variable in input array with : to avoid encoding
 
-def hasUpdatePermissions(order):
-    return CheckWitness(order.senderKey) or CheckWitness(order.receiverKey) or CheckWitness(order.middleManKey)
+def hasUpdatePermissions(asset):
+    return CheckWitness(asset.senderKey) or CheckWitness(asset.receiverKey) or CheckWitness(asset.middleManKey)
